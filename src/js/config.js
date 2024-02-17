83f04ac41f253c3998f1509d7f2308419e394a60
Ahliman-sl <ahliman.slymn@gmail.com>
2024-02-18 00:37:54 +0400

    Initial Commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '5e42123d-5e58-4920-954b-7048653c7080';
+export const MODAL_CLOSE_SEC = 2.5;
